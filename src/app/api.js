c08e5e22218724fa30f007c9a02c09105627580d
umair-fabtechsol <umair.athar@fabtechsol.com>
2024-11-21 22:15:09 +0500

    addd env

--- src/app/api.js
+++ src/app/api.js
@@ -1,3 +1,3 @@
-export const baseuRL = "http://pakcarbazar-env-2.eba-nt2pfmcy.ap-south-1.elasticbeanstalk.com"
+export const baseuRL = process.env.RESCT_APP_BACK_END_URL
 // export const baseuRL = "http://localhost:4000"
 // const baseuRL = "https://arabic-application-01.vercel.app"
